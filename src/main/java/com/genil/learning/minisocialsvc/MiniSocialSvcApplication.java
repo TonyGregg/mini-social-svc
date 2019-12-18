76f097583cff3657102d070c98a46c3633b242cc
TonyGregg <antonygenil@gmail.com>
2019-12-18 12:11:27 -0600

    Initial commit.

---
+++ src/main/java/com/genil/learning/minisocialsvc/MiniSocialSvcApplication.java
@@ -0,0 +1,13 @@
+package com.genil.learning.minisocialsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSocialSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSocialSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genil/learning/minisocialsvc/MiniSocialSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genil.learning.minisocialsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSocialSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
